8eeca6f582428404ded2f014876f357fcc2df9cc
meta-Richa-Jain <41181602+meta-Richa-Jain@users.noreply.github.com>
2018-07-20 00:19:15 +0530

    with some changes

--- SCF-Assignment3/Question3/Shapes.java
+++ SCF-Assignment3/Question3/Shapes.java
@@ -7,22 +7,28 @@ import java.util.Scanner;
 public class Shapes {
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter your choice: \n"
-				+ " 1 to find area of triangle\n"
-				+ " 2 to find area of rectangle\n"
-				+ " 3 to find area of square\n" + " 4 to find area of circle\n"
-				+ " 5 to exit");
-		int choice = scan.nextInt();
+		Area area;
+		double width;
+		double height;
+		double side;
+		double radius; 
+		int choice;
 		do {
+			System.out.println("Enter your choice: \n"
+					+ " 1 to find area of triangle\n"
+					+ " 2 to find area of rectangle\n"
+					+ " 3 to find area of square\n" + " 4 to find area of circle\n"
+					+ " 5 to exit");
+			choice = scan.nextInt();
 			switch (choice) {
 			case 1:
 				try {
 					System.out.println("Enter width and height of triangle");
-					double widthTriangle = scan.nextInt();
-					double heightTriangle = scan.nextInt();
-					Area triAreaObj = new Area();
-					System.out.println(triAreaObj.areaTriangle(widthTriangle,
-							heightTriangle));
+					width = scan.nextInt();
+					height= scan.nextInt();
+					area = new Area();
+					System.out.println("Area : " + area.areaTriangle(width,
+							height));
 				} catch (ArithmeticException e) {
 					System.out.println("Error detected : " + e.getMessage());
 
@@ -31,11 +37,11 @@ public class Shapes {
 			case 2:
 				try {
 					System.out.println("Enter width and height of rectangle");
-					double widthRectangle = scan.nextInt();
-					double heightRectangle = scan.nextInt();
-					Area recAreaObj = new Area();
-					System.out.println(recAreaObj.areaRectangle(widthRectangle,
-							heightRectangle));
+					width = scan.nextInt();
+					height = scan.nextInt();
+					area = new Area();
+					System.out.println("Area : " +area.areaRectangle(width,
+							height));
 				} catch (ArithmeticException e) {
 					System.out.println("Error detected : " + e.getMessage());
 
@@ -44,9 +50,9 @@ public class Shapes {
 			case 3:
 				try {
 					System.out.println("Enter the side of square");
-					double side = scan.nextInt();
-					Area squareAreaObj = new Area();
-					System.out.println(squareAreaObj.areaSquare(side));
+					side = scan.nextInt();
+					area = new Area();
+					System.out.println("Area : " +area.areaSquare(side));
 				} catch (ArithmeticException e) {
 					System.out.println("Error detected : " + e.getMessage());
 
@@ -55,9 +61,9 @@ public class Shapes {
 			case 4:
 				try {
 					System.out.println("Enter radius of the circle");
-					double radius = scan.nextInt();
-					Area circleAreaObj = new Area();
-					System.out.println(circleAreaObj.areaCircle(radius));
+					radius = scan.nextInt();
+					area = new Area();
+					System.out.println("Area : " +area.areaCircle(radius));
 				} catch (ArithmeticException e) {
 					System.out.println("Error detected : " + e.getMessage());
 
