4264037c758dd9205c2bd17e1db0243517c6aede
meta-Richa-Jain <41181602+meta-Richa-Jain@users.noreply.github.com>
2018-07-29 21:57:56 +0530

    Added LCM test cases and improved name

--- Question1/MathematicalFormulasTest.java
+++ Question1/MathematicalFormulasTest.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -9,22 +9,43 @@ public class MathematicalFormulasTest {
 	private int result;
 	
 	@Test
-	public void test1() {
+	public void testHcfFirstGreaterSecondSmaller() {
 		result = formula.findHcf(12, 8);
 		assertEquals(4,result);
 	}
 	
 	@Test
-	public void test2() {
+	public void testHcfFirstNumberZero() {
 		result = formula.findHcf(0, 8);
 		assertEquals(8,result);
 	}
 	
 	@Test
-	public void test3() {
+	public void testHcfSecondNumberZero() {
 		result = formula.findHcf(12, 0);
 		assertEquals(12,result);
 	}
+	
+	@Test
+	public void testHcfFirstSmallerSecondGreater() {
+		result = formula.findHcf(8, 12);
+		assertEquals(4,result);
+	}
+	
+
+	
+	@Test
+	public void testLcmFirstGreaterSecondSmaller() {
+		MathematicalFormulas math = new MathematicalFormulas();
+		result = math.findLcm(4, 3);
+		assertEquals(12,result);
+	}
+	
+//	@Test
+//	public void testLcmFirstSmallerSecondGreater() {
+//		result = formula.findLcm(6, 7);
+//		assertEquals(42,result);
+//	}
 	
 
 }
