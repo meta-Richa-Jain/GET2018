0a1896d6d3220fc6eb4572edbec42ebd584b0427
meta-Richa-Jain <41181602+meta-Richa-Jain@users.noreply.github.com>
2018-08-13 18:38:52 +0530

    Update EmployeeCollection.java

--- Question1/EmployeeCollection.java
+++ Question1/EmployeeCollection.java
@@ -8,52 +8,78 @@ import java.util.Set;
 
 /**
  * This class is used to add employees and sort them by their id or name
+ * 
  * @author Richa Jain
  *
  */
 public class EmployeeCollection {
-	
+
 	List<Employee> employeeList = new ArrayList<Employee>();
 	private Set<Integer> empIdList = new HashSet<Integer>();
-	
+
 	/**
 	 * Method to add employee to employeeList
+	 * 
 	 * @param empId
 	 * @param name
 	 * @param address
 	 * @return true if no duplicate id is present else false
-	 * @throws InvalidInputError if employee id is negative or zero, or name or address is null
+	 * @throws InvalidInputError
+	 *             if employee id is negative or zero, or name or address is
+	 *             null
 	 */
-	public boolean addEmployee(int empId, String name, String address) throws InvalidInputError{
-		try{
-		if(empId<=0 || name == null || address == null){
-			throw new InvalidInputError("Sorry can't add employee with invalid input");
+	public boolean addEmployee(int empId, String name, String address)
+			throws InvalidInputError {
+		try {
+			if (empId <= 0 || name == null || address == null) {
+				throw new InvalidInputError(
+						"Sorry can't add employee with invalid input");
+			}
+			if (!empIdList.contains(empId)) {
+				employeeList.add(new Employee(empId, name, address));
+				empIdList.add(empId);
+				return true;
+			} else {
+				return false;
+			}
+		} catch (InvalidInputError message) {
+			throw new InvalidInputError(
+					"Sorry can't add employee with invalid input");
 		}
-		if(!empIdList.contains(empId)){
-			employeeList.add(new Employee(empId, name, address));
-			empIdList.add(empId);
-			return true;
-		} else {
-			return false;
-		}
-		}catch(InvalidInputError message){
-			throw new InvalidInputError("Sorry can't add employee with invalid input");
-		}
-		
+
 	}
-	
+
 	/**
-	 * Method calls compare method of SortById class to sort employees on the bassis of id
+	 * Method calls compare method of SortById class to sort employees on the
+	 * basis of id
 	 */
-	public void sortEmployeeById(){
+	public void sortEmployeeById() {
 		Collections.sort(employeeList, new SortById());
 	}
-	
+
 	/**
-	 * Method calls compare method of SortByName class to sort employees on the bassis of name
+	 * Method calls compare method of SortByName class to sort employees on the
+	 * basis of name
 	 */
-	public void sortEmployeeByName(){
+	public void sortEmployeeByName() {
 		Collections.sort(employeeList, new SortByName());
+	}
+
+	/**
+	 * Method to return text value of the list
+	 * 
+	 * @param listOfEmployees, containing information about 
+	 * @return text value of the string
+	 */
+	public String toString(List<Employee> listOfEmployees) {
+		StringBuilder result = new StringBuilder();
+		for (int count = 0; count < listOfEmployees.size(); count++) {
+			Employee employee = listOfEmployees.get(count);
+			result.append(employee.getEmpId() + " " + employee.getName() + " "
+					+ employee.getAddress());
+		}
+		return result.toString();
+
 	}
 
 }
