4da7a1db70b284f608a09db8fb77e88239145280
meta-Richa-Jain <41181602+meta-Richa-Jain@users.noreply.github.com>
2018-09-05 16:08:48 +0530

    Update Main.java

--- SpringEx-Collection/src/com/metacube/training/Main.java
+++ SpringEx-Collection/src/com/metacube/training/Main.java
@@ -11,15 +11,23 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-	        CollectionExamples ce=(CollectionExamples)context.getBean("collectionExamples");
 		
- 		List addressList = ce.getAddressList();
-	   	System.out.println(addressList);
-	    	Set addressSet = ce.getAddressSet();
-	   	System.out.println(addressSet);
-	    	Map addressMap = ce.getAddressMap();
-	   	System.out.println(addressMap);
+		
+		try {
+			ApplicationContext context = new ClassPathXmlApplicationContext(
+					"beans.xml");
+			CollectionExamples ce = (CollectionExamples) context
+					.getBean("collectionExamples");
+
+			List addressList = ce.getAddressList();
+			System.out.println(addressList);
+			Set addressSet = ce.getAddressSet();
+			System.out.println(addressSet);
+			Map addressMap = ce.getAddressMap();
+			System.out.println(addressMap);
+		} catch (Exception e) {
+			System.out.println("Class or bean factory not found");
+		}
 	}
 
 
