abfc03a2a3350e84889c9e62a47f064c5476ad01
meta-Richa-Jain <41181602+meta-Richa-Jain@users.noreply.github.com>
2018-09-05 16:17:15 +0530

    Update MainApp.java

--- SpringEx-ScopePrototype/src/com/metacube/training/MainApp.java
+++ SpringEx-ScopePrototype/src/com/metacube/training/MainApp.java
@@ -6,15 +6,28 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		ScopeCheck sc1 = (ScopeCheck) context.getBean("scopeCheck");
+		
+		
+		ScopeCheck sc1 = null;
+		ScopeCheck sc2 = null;
+		try {
+			ApplicationContext context = new ClassPathXmlApplicationContext(
+					"beans.xml");
+			ScopeCheck sc1 = (ScopeCheck) context.getBean("scopeCheck");
+			sc1.setScope("prototype");
+			System.out.println(sc1.getScope());
+			
+			ScopeCheck sc2 = (ScopeCheck) context.getBean("scopeCheck");
+			// need to set every time
+			System.out.println(sc2.getScope());
+			
+		} catch (Exception e) {
+			System.out.println("Class not found or bean  not found");
+		}
+		
+		
 
-		sc1.setScope("prototype");
-		System.out.println(sc1.getScope());
-
-		// need to set every time
-		ScopeCheck sc2 = (ScopeCheck) context.getBean("scopeCheck");
-		System.out.println(sc2.getScope());
+		
 	}
 
 }
