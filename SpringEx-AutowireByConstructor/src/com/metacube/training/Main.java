cee3ad12ad539a47a1fe1b7117b1e9da924193c1
meta-Richa-Jain <41181602+meta-Richa-Jain@users.noreply.github.com>
2018-09-05 16:12:49 +0530

    Update Main.java

--- SpringEx-AutowireByConstructor/src/com/metacube/training/Main.java
+++ SpringEx-AutowireByConstructor/src/com/metacube/training/Main.java
@@ -7,9 +7,15 @@ public class Main {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
-		TextEditor textEditor = (TextEditor) context.getBean("textEditor");
+		TextEditor textEditor = null;
+		try {
+			ApplicationContext context = new ClassPathXmlApplicationContext(
+					"beans.xml");
+			textEditor = (TextEditor) context.getBean("textEditor");
+			
+		} catch (Exception e) {
+			System.out.println("Class not found or bean  not found");
+		}
 		textEditor.spellCheck();
 	}
 
